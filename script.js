3c0ebff66efc2be1c99659db64b1eb38c3b70509
Sachin_Belle <Sachin.s.belle@gmail.com>
2025-02-15 13:39:32 +0530

    final project

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ let player_total_score=document.querySelector(".player_score");
 let bot_total_score=document.querySelector(".bot_score");
 let bot_score=0;
 let user_score=0;
-
+let exit_button=document.querySelector(".exit_game")
 const resetBorders = () => {
     options.forEach((opt) => {
         opt.style.border = "none";
     });
+    display.innerText="Let's Play";
+    display.style.backgroundColor="rgb(120, 120, 176)";
+    display.style.color="white";
 };
 const disable = (flag) => {
     options.forEach((opt) => {
@@ -23,19 +26,39 @@ const disable = (flag) => {
     });
 };
 const check_winner=(user_choose,bot_choose)=>{
+    let user_choose_name;
+    let bot_choose_name;
+    if(user_choose==0) user_choose_name="Rock";
+    else if(user_choose==1) user_choose_name="Paper";
+    else{
+        user_choose_name="Scissor";
+    }
+    if(bot_choose==0) bot_choose_name="Rock";
+    else if(bot_choose==1) bot_choose_name="Paper";
+    else{
+        bot_choose_name="Scissor";
+    }
+
+
+
     if(user_choose!=bot_choose){
     if((user_choose==0&&bot_choose==2)||(user_choose==1&&bot_choose==0)||(user_choose==2&&bot_choose==1)){
         user_score+=1;
         player_total_score.innerText=user_score;
-        display.innerText="Player win";
+        display.innerText=`You won! ${user_choose_name} beats ${bot_choose_name} `;
+        display.style.backgroundColor="green";
     }
     else{
        bot_score+=1;
         bot_total_score.innerText=bot_score;
-        display.innerText="Bot win";
+        display.innerText=`You lost! ${bot_choose_name} beats ${user_choose_name}`;
+        display.style.backgroundColor="red";
     }}
     else{
-        display.innerText="Draw";
+        display.innerText=`Match Draw ${user_choose_name}  equals ${bot_choose_name}`;
+        display.style.color="black";
+        display.style.backgroundColor="white";
+       
     }
 
 
@@ -61,7 +84,9 @@ options.forEach((opt)=>{
             options[randomIndex].style.borderBottom="10px solid green";
             options[randomIndex].style.borderRight="10px solid green";
         }
+        
         check_winner(selected,randomIndex);
+        setTimeout(resetBorders, 2000);
     })
 })
 
@@ -75,3 +100,7 @@ reset.addEventListener("click",()=>{
 })
 
 
+exit_button.addEventListener("click",()=>{
+
+    window.location.href = "second.html";
+})
